d79597ebfa16f38a668a2c98c4ade26d1a1406ae
suf karmon <sufkarmon2@gmail.com>
2024-02-18 19:22:15 +0200

    fix data frequency generator and limit it

--- data_poc.py
+++ data_poc.py
@@ -13,43 +13,31 @@ grouped_by_author_id = df_selected.groupby('author_id')
 #     print(group_df)
 #     print()
 
+# Get the top authors with the most rows
 author_counts = df_selected['author_id'].value_counts()
 top_authors = author_counts.head(1)
 print("Top author_ids with the most rows:")
 for author_id, count in top_authors.items():
     print(f"Author ID: {author_id}, Number of Rows: {count}")
+
+# Filter the DataFrame to include only data from the top authors
+num_rows = 100
 top_authors_data = df_selected[df_selected['author_id'].isin(top_authors.index)]
-# print(top_authors_data)
+top_authors_data = top_authors_data.head(num_rows)  # Limit to top rows
 
-
-
-# Tokenize text and count frequencies for each document (ID)
-word_counts = Counter()
-for text in df['text']:
-    # Split text into tokens using whitespace and punctuation as delimiters
-    tokens = re.findall(r'\b\w+\b', text, flags=re.UNICODE)
-    word_counts.update(tokens)
-
-# Select the top n most frequent words
-n = 10
-top_words = word_counts.most_common(n)
-top_words_set = set(word for word, _ in top_words)
-
-# Select only the required columns
-df_selected = df[['id', 'text']]
-
-# Tokenize text and count frequencies for each document (ID) for only the top n words
+# Tokenize text and count frequencies for each document (ID) in top authors data
 document_elements = []
-for document, text in zip(df_selected['id'], df_selected['text']):
+for document, text in zip(top_authors_data['id'], top_authors_data['text']):
+    # Split text into tokens using whitespace and punctuation as delimiters
     tokens = re.findall(r'\b\w+\b', text, flags=re.UNICODE)
     word_counts = Counter(tokens)
     for element, frequency_in_document in word_counts.items():
-        if element in top_words_set:
-            document_elements.append((document, element, float(frequency_in_document)))
+        document_elements.append((document, element, float(frequency_in_document)))
 
 # Create a DataFrame from the list of tuples
 result_df = pd.DataFrame(document_elements, columns=['document', 'element', 'frequency_in_document'])
 
 # Write the DataFrame to a new CSV file
-result_df.to_csv('test_data/post_frequency.csv', index=False)
-print(f"CSV file 'post_frequency.csv' with only the top {n} elements has been created successfully.")
+result_df.to_csv('test_data/post_frequency.csv', index=False, mode='w', header=True)
+
+print(f"CSV file 'post_frequency.csv' with only the top elements from top authors has been created successfully.")
